dc579889e930c07de68bb72a3ef17516e45e0338
ZeniusX <villacarlos.jb@gmail.com>
2025-03-23 17:23:22 +0800

    update

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -15,6 +16,36 @@ public class MainManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+        }
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    private class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData jsonData = new SaveData();
+        jsonData.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(jsonData);
+        
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData jsonData = JsonUtility.FromJson<SaveData>(json);
+            
+            TeamColor = jsonData.TeamColor;
         }
     }
 }
